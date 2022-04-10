bb6c80e3c14a64277f0174c311166b668e448d1a
reddieabhi <91117253+reddieabhi@users.noreply.github.com>
2022-04-10 11:43:55 +0530

    Create FlipBits.py

---
+++ arrays/kadane's/FlipBits.py
@@ -0,0 +1,21 @@
+### 
+
+
+def flipBits(arr, n):
+    ones_count = 0
+    curr = 0
+    curr_max = 0
+    for i in range(n):
+        if arr[i] == 1:
+            ones_count += 1
+            arr[i] = -1
+        if arr[i] == 0:
+            arr[i] = 1
+        else:
+            arr[i] = -1
+        curr += arr[i]
+        curr_max = max(curr_max, curr)
+        if curr < 0:
+            curr = 0
+
+    return ones_count + curr_max
